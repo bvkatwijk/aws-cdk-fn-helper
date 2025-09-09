443687b8ab7f1caa1508e73899c8ca2d748d4752
Boris van Katwijk <bvkatwijk@gmail.com>
2025-09-09 08:59:16 +0200

    gather methods

--- gen/src/main/java/nl/bvkatwijk/awscdkfnhelper/Generator.java
+++ gen/src/main/java/nl/bvkatwijk/awscdkfnhelper/Generator.java
@@ -1,11 +1,16 @@
 package nl.bvkatwijk.awscdkfnhelper;
 
+import io.vavr.Function1;
+import io.vavr.collection.List;
 import lombok.SneakyThrows;
+import org.apache.commons.lang3.StringUtils;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Generator {
+    private static final String IND = "\t";
+
     public static void main(String[] args) {
         new Generator().run();
     }
@@ -14,8 +19,46 @@ public class Generator {
     private void run() {
         var path = Path.of(this.getClass().getResource("Fn.java").toURI());
         var file = Files.readString(path);
+        var methods = extractMethods(file);
     }
 
-    public record Method() { }
-    public record MethodSource() { }
+    private List<Method> extractMethods(String file) {
+        return List.of(file.split("(\\r?\\n){2,}"))
+            .filter(it -> it.contains("public static "))
+            .map(it -> new MethodSource(List.of(it.split("(\\r?\\n)")).map(String::trim)))
+            .map(MethodSource::toMethod)
+            .toList();
+    }
+
+    public record MethodSource(List<String> source) {
+        public Method toMethod() {
+            return new Method(
+                extractName(source)
+            );
+        }
+
+        private String extractName(List<String> source) {
+            return Function1.<List<String>>identity()
+                .andThen(this::declaration)
+                .andThen(it -> StringUtils.substringBefore(it, "("))
+                .andThen(it -> StringUtils.substringAfterLast(it, " "))
+                .apply(source);
+        }
+
+        private String declaration(List<String> source) {
+            return source
+                .find(it -> it.startsWith("public static"))
+                .getOrElseThrow(() -> new IllegalArgumentException("No public static in " + source));
+        }
+
+
+    }
+
+    public record Method(
+        String name
+        ) { }
+
+    public static String indent(String s) {
+        return IND + s;
+    }
 }
