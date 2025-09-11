af9be3944f1eac9cd2593275a7f5f2831bbad41e
Boris van Katwijk <bvkatwijk@gmail.com>
2025-09-11 16:28:14 +0200

    Update FnLocalTest.java

--- lib/src/test/java/nl/bvkatwijk/awscdkfnhelper/FnLocalTest.java
+++ lib/src/test/java/nl/bvkatwijk/awscdkfnhelper/FnLocalTest.java
@@ -8,16 +8,24 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FnLocalTest {
-	public final FnLocal fn = new FnLocal();
+    public final FnLocal fn = new FnLocal();
 
-	@Nested
-	class SplitDelimiterSourceTest {
-		@Test
-		void canonical() {
-			assertEquals(
-				List.of("tg", "abc-123", "def-456"),
-				fn.split("/", "tg/abc-123/def-456")
-			);
-		}
-	}
+    @Nested
+    class SplitDelimiterSourceTest {
+        @Test
+        void canonical_0() {
+            assertEquals(
+                List.of("a", "b", "c"),
+                fn.split("|", "a|b|c")
+            );
+        }
+
+        @Test
+        void canonical_1() {
+            assertEquals(
+                List.of("a", "", "c", ""),
+                fn.split("|", "a||c|")
+            );
+        }
+    }
 }
